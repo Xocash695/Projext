f9f1c268deb34151008961517663e712dcbcb394
Xocash695 <akash.kallumkal@gmail.com>
2023-04-05 20:48:49 -0400

    nearly finished

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![allow(unused)]
 // witten in rust programming language
-use std::process::{Command, Child, ExitStatus};
+use std::process::{Command, Child, ExitStatus, Stdio};
 extern crate cfonts;
 use cfonts::{ say, Options, Fonts };
 use tts_rust::{ tts::GTTSClient, languages::Languages };
 use indicatif::ProgressBar;
 use dialoguer::{console::Term, theme::ColorfulTheme, Select};
+use std::env;
+use std::fs;
+use execute::{Execute, shell};
+use dialoguer::Confirm;
 
 fn main()  {
     let mut child;
@@ -15,7 +19,20 @@ fn main()  {
     child = Command::new("sleep").arg("0.4").spawn().unwrap();
     result = child.wait().unwrap();
     selector();
+    if Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Do you want to continue using selector or end program?")
+        .interact()
+        .unwrap()
+    {
+        println!("Looks like you want to continue using selector");
+        selector();
+        goodbye()
 
+    } else {
+        println!("alright then :(");
+        goodbye();
+    }
+    
 }
 
 fn boot() {
@@ -23,8 +40,8 @@ fn boot() {
     let mut child:Child;
     let mut result:ExitStatus; 
     println!("PROGRAM STARTING UP");
-    let bar = ProgressBar::new(1000);
-     for _ in 0..1000 {
+    let bar = ProgressBar::new(100);
+     for _ in 0..100 {
          bar.inc(1);
          child = Command::new("sleep").arg("0.01").spawn().unwrap();
          result = child.wait().unwrap();
@@ -39,12 +56,10 @@ fn boot() {
         println!("Width: {}\nHeight: {}", w, h);
         child = Command::new("sleep").arg("0.4").spawn().unwrap();
         result = child.wait().unwrap();
-        println!("optimzing for terminal size");
       } else {
         println!("Unable to get term size :(");
         child = Command::new("sleep").arg("0.4").spawn().unwrap();
         result = child.wait().unwrap();
-        println!("Terminal size is unoptimized");
     }
     child = Command::new("sleep").arg("0.4").spawn().unwrap();
     result = child.wait().unwrap();
@@ -60,7 +75,7 @@ fn logo() {
             } else{
                 print!("@");
             }
-            let mut child = Command::new("sleep").arg("0.001").spawn().unwrap();
+            let mut child = Command::new("sleep").arg("0.01").spawn().unwrap();
             let result = child.wait().unwrap();
         }
         println!("");
@@ -77,20 +92,83 @@ fn logo() {
     };
     narrator.speak("Welcome to Projext");
 }
-fn selector() -> std::io::Result<()>{
-    let items = vec!["Launch android phone (Requires android studio)", "Launch Iphone (Requires system running MacOS and Xcode)", "Make website (Requires Docker)", "Make AI"];
+fn selector() {
+    let mut narrator: GTTSClient = GTTSClient {
+        volume: 1.0, 
+        language: Languages::English, // use the Languages enum
+        tld: "com",
+    };
+    narrator.speak("what do you want to do");
+    narrator.speak("Options are listed below");
+    let selections = &[
+        "Launch Iphone",
+        "Launch Android phone",
+        "show Feet pic",
+    ];
     let selection = Select::with_theme(&ColorfulTheme::default())
-        .items(&items)
-        .default(0)
         .with_prompt("What do you want to do?")
-        .interact_on_opt(&Term::stderr())?;
-        
-
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+    println!("Okay  {}!", selections[selection]);
+    let message = "Okay doing";
+    let sayresult = [message, selections[selection]].join("\n");
+    narrator.speak(&sayresult);
     match selection {
-        Some(index) => println!("User selected item : {}", items[index]),
-        None => println!("User did not select anything")  
+        0 => iphone(),
+        1 => android(),
+        2 => feet(),
+        _ => print!("You didn't select anything"),
     }
 
+}
 
-    Ok(())
+fn feet() { 
+    let filepath = "feet.txt";
+    let contents = fs::read_to_string(filepath)
+    .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+
+fn android() {
+    let mut command;
+
+    command = shell("cd ~/Library/Android/sdk/emulator");
+    
+    command.stdout(Stdio::piped());
+
+    command.execute_output().unwrap();
+
+    command = shell("./emulator -avd Pixel_6_Pro_API_UpsideDownCake");
+    
+    command.stdout(Stdio::piped());
+
+    command.execute_output().unwrap();
+
+}
+
+fn iphone() {
+    let mut command;
+
+    command = shell("open -a Simulator --args -CurrentDeviceUDID 936604F6-449D-4373-B8AD-94C7D08A7777");
+    
+    command.stdout(Stdio::piped());
+
+    command.execute_output().unwrap();
+
+}
+fn goodbye() {
+    say(Options {
+        text: String::from("Thank you and Goodbye"),
+        font: Fonts::FontTiny,
+        ..Options::default()
+    });
+    let mut narrator: GTTSClient = GTTSClient {
+        volume: 1.0, 
+        language: Languages::English, // use the Languages enum
+        tld: "com",
+    };
+    narrator.speak("Thank you and Goodbye");
 }
